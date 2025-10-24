4da1e4f5c4cb15b75116748420e7a3a6e61c7c91
Anis Mulla <135443188+AnisMulla9740@users.noreply.github.com>
2025-10-24 17:53:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio-Anis-Mulla/',  // MUST match your repo name
+  base: '/',  // root path for Vercel
   plugins: [react()]
 });
