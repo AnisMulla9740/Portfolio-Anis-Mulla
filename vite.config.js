35bbc9158464e778b47e5331436035d9759d02a5
Anis Mulla <135443188+AnisMulla9740@users.noreply.github.com>
2025-10-24 17:39:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio-Anis-Mulla/',  // MUST match repo name exactly
+  base: '/Portfolio-Anis-Mulla/',  // MUST match your repo name
   plugins: [react()]
 });
